2846e170724e56804fa1df464af363f8d10b8552
aopisco <angela.pisco@czbiohub.org>
2020-06-18 00:02:27 -0700

    cell fractions plot

--- utils/general_utils.py
+++ utils/general_utils.py
@@ -25,3 +25,65 @@ def convert_ensembl_symbol(adata,species='human',idcol = 'ensembls'):
         'ensembl_id').set_index('symbol')
     
     return adata
+
+# transform all categorical columns into strings
+def remove_cats(adata):
+
+    cat_columns = adata.obs.select_dtypes(['category']).columns
+    adata.obs[cat_columns] = adata.obs[cat_columns].astype(str)
+    
+    return adata
+
+# balanced downsampling 
+import logging as logg
+def downsample_to_smallest_category(
+        adata,
+        column="sample_short",
+        random_state=None,
+        min_cells=15,
+        keep_small_categories=False
+) -> sc.AnnData:
+    """
+    returns an annData object in which all categories in 'column' have
+    the same size
+
+    column
+        column with the categories to downsample
+    min_cells
+        Minimum number of cells to downsample.
+        Categories having less than `min_cells` are discarded unless
+        keep_small_categories is True
+    keep_small_categories
+        Be default categories with less than min_cells are discarded.
+        Set to true to keep them
+    """
+    counts = adata.obs[column].value_counts(sort=False)
+    if len(counts[counts < min_cells]) > 0 and keep_small_categories is False:
+        logg.warning(
+            "The following categories have less than {} cells and will be "
+            "ignored: {}".format(min_cells, dict(counts[counts < min_cells]))
+        )
+    min_size = min(counts[counts >= min_cells])
+    sample_selection = None
+    for sample, num_cells in counts.items():
+        if num_cells <= min_cells:
+            if keep_small_categories:
+                sel = adata.obs.index.isin(
+                    adata.obs[adata.obs[column] == sample].index)
+            else:
+                continue
+        else:
+            sel = adata.obs.index.isin(
+                adata.obs[adata.obs[column] == sample]
+                .sample(min_size, random_state=random_state)
+                .index
+            )
+        if sample_selection is None:
+            sample_selection = sel
+        else:
+            sample_selection |= sel
+    logg.info(
+        "The cells in category {!r} had been down-sampled to have each {} cells. "
+        "The original counts where {}".format(column, min_size, dict(counts))
+    )
+    return adata[sample_selection].copy()
