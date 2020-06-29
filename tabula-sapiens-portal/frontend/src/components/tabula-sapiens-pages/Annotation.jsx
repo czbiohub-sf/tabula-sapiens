9629619bf37d93dfa1411a1bba6c318b8a2f251b
aopisco <angela.oliveira.pisco@gmail.com>
2020-06-28 19:59:24 -0700

    upload frontend

---
+++ tabula-sapiens-portal/frontend/src/components/tabula-sapiens-pages/Annotation.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+export default class AnnotationsUpdate extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onFileChange = this.onFileChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      profileImg: "",
+    };
+  }
+
+  onFileChange(e) {
+    this.setState({ profileImg: e.target.files[0] });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("profileImg", this.state.profileImg);
+    axios
+      .post("http://localhost:3000/api/user-profile", formData, {})
+      .then((res) => {
+        console.log(res);
+      });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <form onSubmit={this.onSubmit}>
+            <div className="form-group">
+              <input type="file" onChange={this.onFileChange} />
+            </div>
+            <div className="form-group">
+              <button className="btn btn-primary" type="submit">
+                Upload
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
