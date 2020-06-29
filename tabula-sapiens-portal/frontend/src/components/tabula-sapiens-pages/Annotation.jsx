090787929a258af48028d4a257ef5bf6e7a0b72a
aopisco <angela.oliveira.pisco@gmail.com>
2020-06-29 08:48:07 -0700

    updating Annotation.jsx

--- tabula-sapiens-portal/frontend/src/components/tabula-sapiens-pages/Annotation.jsx
+++ tabula-sapiens-portal/frontend/src/components/tabula-sapiens-pages/Annotation.jsx
@@ -1,7 +1,11 @@
 import React, { Component } from "react";
 import axios from "axios";
 
-import { BiohubUI, Button, Link as CZUILink } from "cz-ui";
+// import { BiohubUI, Button, Link as CZUILink } from "cz-ui";
+import Fab from "@material-ui/core/Fab";
+import AddIcon from "@material-ui/icons/Add";
+import Button from "@material-ui/core/Button";
+import CloudUploadIcon from "@material-ui/icons/CloudUpload";
 
 export default class AnnotationsUpdate extends Component {
   constructor(props) {
@@ -32,18 +36,60 @@ export default class AnnotationsUpdate extends Component {
 
   render() {
     return (
-      <div className="container">
-        <div className="row">
-          <form onSubmit={this.onSubmit}>
-            <div className="form-group">
-              <input type="file" onChange={this.onFileChange} />
-            </div>
-            <div className="form-group">
-              <button className="btn btn-primary" type="submit">
+      <div>
+        <Heading title="About" />
+        <Container maxWidth="lg">
+          <Box my={4}>
+            <Typography
+              variant="h6"
+              component="h3"
+              gutterBottom
+              align="justify"
+            >
+              Add your annotations here <p />
+              Layout very confusing
+              <p />
+              We are getting there
+            </Typography>
+          </Box>
+        </Container>
+
+        <Heading title="Upload annotations" />
+
+        <img src={"../../images/annotations.png"} width="1600" />
+
+        <div className="container">
+          <div className="row">
+            <form onSubmit={this.onSubmit}>
+              <div className="form-group">
+                {/* <input type="file" onChange={this.onFileChange} /> */}
+
+                <input
+                  ref={"file-upload"}
+                  type="file"
+                  onChange={this.onFileChange}
+                />
+                <Button
+                  onClick={(e) => {
+                    this.refs["file-upload"].click();
+                  }}
+                ></Button>
+              </div>
+
+              <div className="form-group">
+                {/* <button className="btn btn-primary" type="submit">
                 Upload
-              </button>
-            </div>
-          </form>
+              </button> */}
+                <Button
+                  variant="contained"
+                  color="default"
+                  startIcon={<CloudUploadIcon />}
+                >
+                  Upload
+                </Button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     );
