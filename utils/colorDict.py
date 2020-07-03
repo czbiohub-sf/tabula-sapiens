047ca3a02c2ccd05e95cc4c7f6690e957f231629
aopisco <angela.pisco@czbiohub.org>
2020-07-03 12:44:28 -0700

    utilities scripts

---
+++ utils/colorDict.py
@@ -0,0 +1,106 @@
+import matplotlib.pyplot as plt
+
+# create a color dictionary for tissues
+def tissue_color_dict():
+    
+    color_dict = {'Bladder': '#e7969c',
+             'Blood': '#d6616b',
+             'Bone_Marrow': '#cedb9c',
+             'Fat': '#e7cb94',
+             'Heart': '#637939',
+             'Kidney': '#7b4173',
+             'Large_Intestine': '#31a354',
+             'Lung': '#3182bd',
+             'Lymph_Node': '#8c6d31',
+             'Muscle': '#e7ba52',
+             'Pancreas': '#fd8d3c',
+             'Skin': '#ce6dbd',
+             'Small_Intestine': '#6baed6',
+             'Spleen': '#393b79',
+             'Thymus': '#9c9ede',
+             'Trachea': '#969696',
+             'Vasculature': '#843c39'}
+    
+    return color_dict
+
+
+# create a color dictionary for donors
+def donor_color_dict():
+    donors = ['TSP1','TSP2']
+    
+    import matplotlib.colors as pltcolors
+    
+    cmap = plt.cm.get_cmap("YlGnBu")
+        
+    donor_color_dict = {}
+    j=1/len(donors)
+    for d in donors:
+        donor_color_dict[d] = pltcolors.to_hex(cmap(j))
+        j+=1/len(donors)
+        
+    return donor_color_dict
+
+
+# create a color dictionary for methods
+def method_color_dict():
+    methods = ['10X','smartseq2']
+    
+    import matplotlib.colors as pltcolors
+    
+    method_color_dict = {}
+    method_color_dict['10X'] = '#90ee90'
+    method_color_dict['smartseq2'] = '#006400'
+    
+    return method_color_dict
+
+# visualize the color dictionaries
+def plot_colortable(colors, title, sort_colors=True, emptycols=0):
+
+    cell_width = 212
+    cell_height = 22
+    swatch_width = 48
+    margin = 12
+    topmargin = 40
+
+    # Sort colors by hue, saturation, value and name.
+    by_hsv = [(v, k) for k, v in colors.items()]
+    
+    if sort_colors is True:
+        by_hsv = sorted(by_hsv)
+    names = [name for hsv, name in by_hsv]
+
+    n = len(names)
+    ncols = 4 - emptycols
+    nrows = n // ncols + int(n % ncols > 0)
+
+    width = cell_width * 4 + 2 * margin
+    height = cell_height * nrows + margin + topmargin
+    dpi = 72
+
+    fig, ax = plt.subplots(figsize=(width / dpi, height / dpi), dpi=dpi)
+    fig.subplots_adjust(margin/width, margin/height,
+                        (width-margin)/width, (height-topmargin)/height)
+    ax.set_xlim(0, cell_width * 4)
+    ax.set_ylim(cell_height * (nrows-0.5), -cell_height/2.)
+    ax.yaxis.set_visible(False)
+    ax.xaxis.set_visible(False)
+    ax.set_axis_off()
+    ax.set_title(title, fontsize=24, loc="left", pad=10)
+
+    for i, name in enumerate(names):
+        row = i % nrows
+        col = i // nrows
+        y = row * cell_height
+
+        swatch_start_x = cell_width * col
+        swatch_end_x = cell_width * col + swatch_width
+        text_pos_x = cell_width * col + swatch_width + 7
+
+        ax.text(text_pos_x, y, name, fontsize=14,
+                horizontalalignment='left',
+                verticalalignment='center')
+
+        ax.hlines(y, swatch_start_x, swatch_end_x,
+                  color=colors[name], linewidth=18)
+
+    return fig
